014db3f5fc175b1fe798100145b4032e467a81dc
Mojammel Noman <molla.noman23@gmail.com>
2024-05-10 17:17:57 +0600

    login page added

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Helmet>
+        <title>HelpConnect| Login</title>
+      </Helmet>
+
+      <p>login page</p>
+    </>
+  );
+}
--- src/route/Route.jsx
+++ src/route/Route.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
 import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
+import LoginPage from "../pages/LoginPage/LoginPage";
 
 const routes = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const routes = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
       },
     ],
   },

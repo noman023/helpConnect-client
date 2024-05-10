6c85102d99d18b0253b364296bcb2bfb5e4e55fa
Mojammel Noman <molla.noman23@gmail.com>
2024-05-10 17:19:32 +0600

    register page added

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet";
+
+export default function RegisterPage() {
+  return (
+    <>
+      <Helmet>
+        <title>HelpConnect| Register</title>
+      </Helmet>
+
+      <p>register page</p>
+    </>
+  );
+}
--- src/route/Route.jsx
+++ src/route/Route.jsx
@@ -4,6 +4,7 @@ import RootLayout from "../layout/RootLayout";
 import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
 import LoginPage from "../pages/LoginPage/LoginPage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
 
 const routes = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const routes = createBrowserRouter([
       {
         path: "/login",
         element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
       },
     ],
   },

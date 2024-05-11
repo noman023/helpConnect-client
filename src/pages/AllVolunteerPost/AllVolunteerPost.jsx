961c756386eda0c05952bc1f9e1a691d3ac7c6dd
Mojammel Noman <molla.noman23@gmail.com>
2024-05-12 02:39:41 +0600

    card added at all volunteer post page

--- src/pages/AllVolunteerPost/AllVolunteerPost.jsx
+++ src/pages/AllVolunteerPost/AllVolunteerPost.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet";
+import CardComponent from "../../components/Card/Card";
 
 export default function AllVolunteerPost() {
   return (
@@ -7,7 +8,9 @@ export default function AllVolunteerPost() {
         <title>HelpConnect | All Volunteer Posts</title>
       </Helmet>
 
-      <p>AllVolunteerPost page</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
+        <CardComponent />
+      </div>
     </div>
   );
 }

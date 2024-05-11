e4d12b66291df46910c0ba5a4f237890b55efe08
Mojammel Noman <molla.noman23@gmail.com>
2024-05-12 03:43:40 +0600

    added text input field in volunteer post page

--- src/pages/AllVolunteerPost/AllVolunteerPost.jsx
+++ src/pages/AllVolunteerPost/AllVolunteerPost.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from "react-helmet";
+import { TextInput } from "flowbite-react";
+
 import CardComponent from "../../components/Card/Card";
 
 export default function AllVolunteerPost() {
@@ -7,6 +9,10 @@ export default function AllVolunteerPost() {
       <Helmet>
         <title>HelpConnect | All Volunteer Posts</title>
       </Helmet>
+
+      <div className="w-1/3 mx-auto mt-5">
+        <TextInput type="text" placeholder="Search by Post Title" />
+      </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
         <CardComponent />

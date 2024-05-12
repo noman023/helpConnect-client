712cfa005f2c2a7218625b9173262618b94cc8f0
Mojammel Noman <molla.noman23@gmail.com>
2024-05-12 23:29:46 +0600

    all post component showing data format added

--- src/pages/AllVolunteerPost/AllVolunteerPost.jsx
+++ src/pages/AllVolunteerPost/AllVolunteerPost.jsx
@@ -2,21 +2,58 @@ import { Helmet } from "react-helmet";
 import { TextInput } from "flowbite-react";
 
 import CardComponent from "../../components/Card/Card";
+import { useState } from "react";
+import TableComponent from "../../components/Table/Table";
+
+import { BsGrid3X3GapFill } from "react-icons/bs";
+import { FaTableList } from "react-icons/fa6";
 
 export default function AllVolunteerPost() {
+  const [searchText, setsearchText] = useState("");
+  const [isTableLayout, setIsTableLayout] = useState(true);
+
   return (
     <div>
       <Helmet>
         <title>HelpConnect | All Volunteer Posts</title>
       </Helmet>
 
-      <div className="w-1/3 mx-auto mt-5">
-        <TextInput type="text" placeholder="Search by Post Title" />
+      {/* search text */}
+      <div className="md:w-1/3 mx-auto mt-5">
+        <TextInput
+          type="text"
+          placeholder="Search by Post Title"
+          onChange={(e) => setsearchText(e.target.value)}
+        />
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
-        <CardComponent />
+      {/* select layout format*/}
+      <div className="text-center my-5 text-green-600">
+        <p className="text-xl font-bold">Change Layout</p>
+
+        <div className="flex gap-4 text-2xl items-center justify-center mt-2">
+          <BsGrid3X3GapFill
+            title="Card Format"
+            className="cursor-pointer"
+            onClick={() => setIsTableLayout(false)}
+          />
+
+          <FaTableList
+            title="Table Format"
+            className="cursor-pointer"
+            onClick={() => setIsTableLayout(true)}
+          />
+        </div>
       </div>
+
+      {/* if isTableLayout true then show data using table else using card */}
+      {isTableLayout ? (
+        <TableComponent tableUsedIn={"allPost"} />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
+          <CardComponent />
+        </div>
+      )}
     </div>
   );
 }

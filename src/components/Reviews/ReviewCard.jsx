0fa3104901dfa1d148929fc2070219bdac59874f
Mojammel Noman <molla.noman23@gmail.com>
2024-05-13 17:48:25 +0600

    fetchded review data

--- src/components/Reviews/ReviewCard.jsx
+++ src/components/Reviews/ReviewCard.jsx
@@ -1,37 +1,34 @@
 import { Card, Rating } from "flowbite-react";
 import { motion } from "framer-motion";
 
-import cleaning from "../../assets/cleaning.jpg";
+export default function ReviewCard({ data }) {
+  const { userImage, userName, address, review } = data;
 
-export default function ReviewCard() {
   return (
     <motion.div
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
       transition={{ duration: 0.7, ease: "easeInOut" }}
     >
-      <Card className="max-w-sm border-green-600">
+      <Card className="max-w-sm md:h-[300px] border-green-600">
         <div className="flex gap-7">
           <div>
             <img
-              src={cleaning}
+              src={userImage}
               alt="reviewer image"
               className="rounded-br-3xl w-20 h-20"
             />
           </div>
 
           <div className="text-gray-900 dark:text-white">
-            <h5 className="text-2xl font-bold ">John Doe</h5>
+            <h5 className="text-2xl font-bold ">{userName}</h5>
 
-            <p className="text-gray-400">Mirpur, Dhaka</p>
+            <p className="text-gray-400">{address}</p>
           </div>
         </div>
 
         <div className="flex justify-between font-normal text-gray-700 dark:text-gray-400">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem omnis
-          libero vero officiis voluptatibus et laborum, nemo nostrum, nobis
-          animi recusandae, itaque totam sint cumque sunt cum similique possimus
-          error.
+          {review}
         </div>
 
         <div>
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,14 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+
 import SectionHead from "../SectionHead/SectionHead";
 import ReviewCard from "./ReviewCard";
+import SpinnerComponent from "../Spinner/Spinner";
+import axios from "axios";
 
 export default function Reviews() {
+  const { data, isPending } = useQuery({
+    queryKey: ["reviews"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:5000/reviews");
+
+      return res.data;
+    },
+  });
+
   return (
     <>
       <SectionHead>What They're Talking About Us</SectionHead>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
-        <ReviewCard />
-      </div>
+      {isPending ? (
+        <div className="text-center mt-10">
+          <SpinnerComponent />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
+          {data.map((review, idx) => (
+            <ReviewCard data={review} key={idx} />
+          ))}
+        </div>
+      )}
     </>
   );
 }

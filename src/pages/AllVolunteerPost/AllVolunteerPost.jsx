a936c8c6277bf08f5d4fa932182c7a477fbf911f
Mojammel Noman <molla.noman23@gmail.com>
2024-05-13 19:31:51 +0600

    shows all post

--- src/pages/AllVolunteerPost/AllVolunteerPost.jsx
+++ src/pages/AllVolunteerPost/AllVolunteerPost.jsx
@@ -4,13 +4,26 @@ import { TextInput } from "flowbite-react";
 import CardComponent from "../../components/Card/Card";
 import { useState } from "react";
 import TableComponent from "../../components/Table/Table";
-
 import { BsGrid3X3GapFill } from "react-icons/bs";
 import { FaTableList } from "react-icons/fa6";
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import baseUrl from "../../baseUrl";
+import SpinnerComponent from "../../components/Spinner/Spinner";
 
 export default function AllVolunteerPost() {
   const [searchText, setsearchText] = useState("");
   const [isTableLayout, setIsTableLayout] = useState(false);
+
+  const { data, status } = useQuery({
+    queryKey: ["allPost"],
+    queryFn: async () => {
+      const res = await axios.get(`${baseUrl}/posts`);
+
+      return res.data;
+    },
+  });
 
   return (
     <div>
@@ -31,6 +44,7 @@ export default function AllVolunteerPost() {
       <div className="text-center my-5 text-green-600">
         <p className="text-xl font-bold">Change Layout</p>
 
+        {/* card icon */}
         <div className="flex gap-4 text-2xl items-center justify-center mt-2">
           <BsGrid3X3GapFill
             title="Card Format"
@@ -38,6 +52,7 @@ export default function AllVolunteerPost() {
             onClick={() => setIsTableLayout(false)}
           />
 
+          {/* table icon */}
           <FaTableList
             title="Table Format"
             className="cursor-pointer"
@@ -46,13 +61,22 @@ export default function AllVolunteerPost() {
         </div>
       </div>
 
-      {/* if isTableLayout true then show data using table else using card */}
-      {isTableLayout ? (
-        <TableComponent tableUsedIn={"allPost"} />
-      ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
-          <CardComponent />
-        </div>
+      {status === "pending" && <SpinnerComponent />}
+
+      {/* if data is there */}
+      {status === "success" && (
+        <>
+          {/* if isTableLayout true then show data using table else using card */}
+          {isTableLayout ? (
+            <TableComponent tableUsedIn={"allPost"} />
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 my-10">
+              {data.map((post) => (
+                <CardComponent key={post._id} data={post} />
+              ))}
+            </div>
+          )}
+        </>
       )}
     </div>
   );

0f42096fa38dc94c68d8d6f415f89acd95595128
Mojammel Noman <molla.noman23@gmail.com>
2024-05-14 13:42:43 +0600

    shows my volunteer request posts

--- src/pages/MyVolunteerRequest/MyVolunteerRequest.jsx
+++ src/pages/MyVolunteerRequest/MyVolunteerRequest.jsx
@@ -1,14 +1,36 @@
 import { Helmet } from "react-helmet";
 import TableComponent from "../../components/Table/Table";
+import { useContext, useEffect } from "react";
+
+import axios from "axios";
+import baseUrl from "../../baseUrl";
+import { AuthContext } from "../../providers/AuthContext";
+import { useQuery } from "@tanstack/react-query";
+import SpinnerComponent from "../../components/Spinner/Spinner";
 
 export default function MyVolunteerRequest() {
+  const { user } = useContext(AuthContext);
+
+  const { data, isPending } = useQuery({
+    queryKey: ["myPosts"],
+    queryFn: async () => {
+      const res = await axios.get(`${baseUrl}/myRequests?email=${user?.email}`);
+
+      return res.data;
+    },
+  });
+
   return (
     <div>
       <Helmet>
         <title>HelpConnect | My Volunteer Requests</title>
       </Helmet>
 
-      <TableComponent tableUsedIn={"myRequest"} />
+      {isPending ? (
+        <SpinnerComponent />
+      ) : (
+        <TableComponent tableUsedIn={"myRequest"} data={data} />
+      )}
     </div>
   );
 }
